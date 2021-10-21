0d7234d499765a483451ce37fcbb743ad2929e2b
mohammedrafeeqma <mohammedrafeeqma@gmail.com>
2021-10-21 12:50:37 +0530

    youtube clone is completed

---
+++ js/script.js
@@ -0,0 +1,55 @@
+
+$("#video-holder1").mouseenter(function () {
+    document.getElementById('video1').play();
+});
+$("#video-holder1").mouseleave(function () {
+    document.getElementById('video1').pause();
+});
+
+$("#video-holder2").mouseenter(function () {
+    document.getElementById('video2').play();
+});
+$("#video-holder2").mouseleave(function () {
+    document.getElementById('video2').pause();
+});
+
+$("#video-holder3").mouseenter(function () {
+    document.getElementById('video3').play();
+});
+$("#video-holder3").mouseleave(function () {
+    document.getElementById('video3').pause();
+});
+
+$("#video-holder4").mouseenter(function () {
+    document.getElementById('video4').play();
+});
+$("#video-holder4").mouseleave(function () {
+    document.getElementById('video4').pause();
+});
+//  comments
+$("#video-holder5").mouseenter(function () {
+    document.getElementById('video5').play();
+});
+$("#video-holder5").mouseleave(function () {
+    document.getElementById('video5').pause();
+});
+
+$("#video-holder6").mouseenter(function () {
+    document.getElementById('video6').play();
+});
+$("#video-holder6").mouseleave(function () {
+    document.getElementById('video6').pause();
+});
+
+$("#video-holder7").mouseenter(function () {
+    document.getElementById('video7').play();
+});
+$("#video-holder7").mouseleave(function () {
+    document.getElementById('video7').pause();
+});
+$("#video-holder8").mouseenter(function () {
+    document.getElementById('video8').play();
+});
+$("#video-holder8").mouseleave(function () {
+    document.getElementById('video8').pause();
+});
